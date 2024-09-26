93d90fa38b818d208b27ede7f36c1927242ddd0b
DPSS <dafnesan25@gmail.com>
2024-09-26 12:19:27 -0600

    Correcciones en el código otros 2

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,27 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
+const $n = document.querySelector('.name');
+const $b = document.querySelector('.blog');
 const $l = document.querySelector('.location');
 
-function displayUser(username) {
+async function displayUser(username){
   $n.textContent = 'cargando...';
+  try{
   const response = await fetch(`${usersEndpoint}/${username}`);
-  console.log(data);
+  const data = await response.json();
   $n.textContent = '${data.name}';
   $b.textContent = '${data.blog}';
   $l.textContent = '${data.location}';
-}
+  }catch (error){
+    handleError(error);
+  }//catch
+  
 
 function handleError(err) {
   console.log('OH NO!');
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
-}
+  $n.textContent = `Algo salió mal: ${err}`
+}// function handle error
+}// function display
 
 displayUser('stolinski').catch(handleError);
